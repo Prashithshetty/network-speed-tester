dd4f7a701e78612ac2f45c36c38421285d8bdbe8
Prashith Shetty <147428351+Prashithshetty@users.noreply.github.com>
2025-04-21 18:57:04 +0530

    Update network_speed_tester.py

--- network_speed_tester.py
+++ network_speed_tester.py
@@ -14,3 +14,18 @@ DEFAULT_DATA_SIZE = 10 * 1024 * 1024  # 10MB default test size
 DEFAULT_TIMEOUT = 10  # seconds
 DEFAULT_UDP_PACKETS = 1000
 
+class NetworkSpeedTester:
+    
+    
+    def __init__(self, host: str, port: int, buffer_size: int, data_size: int,
+                 timeout: int, protocol: str = 'tcp', verbose: bool = False):
+       
+        self.host = host
+        self.port = port
+        self.buffer_size = buffer_size
+        self.data_size = data_size
+        self.timeout = timeout
+        self.protocol = protocol.lower()
+        self.verbose = verbose
+        self.stop_event = threading.Event()
+        
