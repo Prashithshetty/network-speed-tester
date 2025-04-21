cb6456da0d953a49a8e368935367393eaf9dafc9
Prashith Shetty <147428351+Prashithshetty@users.noreply.github.com>
2025-04-21 21:15:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Default configuration settings for the network speed tester
+
+# Network settings
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 5000
+DEFAULT_BUFFER_SIZE = 8192  # 8KB buffer
+DEFAULT_DATA_SIZE = 10 * 1024 * 1024  # 10MB default test size
+DEFAULT_TIMEOUT = 10  # seconds
+DEFAULT_UDP_PACKETS = 1000
