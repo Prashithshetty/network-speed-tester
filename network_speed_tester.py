74d9f58e880f3f6ae6ea07e5113eaea831f8509f
Prashith Shetty <147428351+Prashithshetty@users.noreply.github.com>
2025-04-21 18:55:52 +0530

    Update network_speed_tester.py

--- network_speed_tester.py
+++ network_speed_tester.py
@@ -5,3 +5,12 @@ import threading
 import random
 import sys
 import statistics
+
+# Default settings
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 5000
+DEFAULT_BUFFER_SIZE = 8192  # 8KB buffer
+DEFAULT_DATA_SIZE = 10 * 1024 * 1024  # 10MB default test size
+DEFAULT_TIMEOUT = 10  # seconds
+DEFAULT_UDP_PACKETS = 1000
+
